162b01caf57811abb245048cb0add4e26abdbc5e
roshpyn <lukasz.szadowski@gmail.com>
2019-06-26 16:53:06 +0200

    Init service

---
+++ src/main/java/psw/lab7/lab7/services/InitService.java
@@ -0,0 +1,17 @@
+package psw.lab7.lab7.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@Slf4j
+public class InitService {
+
+
+    @PostConstruct
+    void init(){
+
+    }
+}
