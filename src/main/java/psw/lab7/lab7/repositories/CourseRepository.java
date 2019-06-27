0fca43a280de41f6f57ec50097d706a5ea1531b9
roshpyn <lukasz.szadowski@gmail.com>
2019-06-27 04:17:00 +0200

    repos fixed and needed methods added to them

--- src/main/java/psw/lab7/lab7/repositories/CourseRepository.java
+++ src/main/java/psw/lab7/lab7/repositories/CourseRepository.java
@@ -3,5 +3,8 @@ package psw.lab7.lab7.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import psw.lab7.lab7.models.Course;
 
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course,Long> {
+    Course findByName(Optional<String> name);
 }
--- src/main/java/psw/lab7/lab7/repositories/UserApplicationRepository.java
+++ src/main/java/psw/lab7/lab7/repositories/UserApplicationRepository.java
@@ -1,7 +1,13 @@
 package psw.lab7.lab7.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import psw.lab7.lab7.models.Course;
+import psw.lab7.lab7.models.User;
 import psw.lab7.lab7.models.UserApplication;
 
+import java.util.List;
+
 public interface UserApplicationRepository extends JpaRepository<UserApplication,Long> {
+    List<UserApplication> findByCourse(Course course);
+    List<UserApplication> findByUser(User user);
 }
